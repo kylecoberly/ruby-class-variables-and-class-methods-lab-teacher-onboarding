74d688667be8b12a7797eecea351895506bed81b
essential-gemfile-5901 <kyle.coberly@gmail.com>
2019-03-03 23:42:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,8 @@
 class Song
   attr_accessor :name, :artist, :genre
-  @@count
-  @@artists
-  @@genres
+  @@count = 0
+  @@artists = []
+  @@genres = []
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
